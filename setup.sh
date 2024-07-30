85bd36d358898ae286bf94c9f7d4858a11eb844c
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-07-30 16:57:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -119,10 +119,10 @@ xfconf-query -c xfwm4 -p /general/placement_ratio -n -t int -s 100
 xfconf-query -c xfwm4 -p /general/show_popup_shadow -n -t bool -s true
 xfconf-query -c xfwm4 -p /general/wrap_windows -n -t bool -s false
 #xfconf-query -c xfce4-panel -p /panels -n -t int -s 1 -a
-#xfconf-query -c xfce4-panel -p /panels/panel-1/size -n -t int -s 32
-#xfconf-query -c xfce4-panel -p /panels/panel-1/icon-size -n -t int -s 16
-#xfconf-query -c xfce4-panel -p /plugins/plugin-1/show-button-title -n -t bool -s false
-#xfconf-query -c xfce4-panel -p /plugins/plugin-1/button-icon -n -t string -s "desktop-environment-xfce"
+xfconf-query -c xfce4-panel -p /panels/panel-1/size -n -t int -s 32
+xfconf-query -c xfce4-panel -p /panels/panel-1/icon-size -n -t int -s 16
+xfconf-query -c xfce4-panel -p /plugins/plugin-1/show-button-title -n -t bool -s false
+xfconf-query -c xfce4-panel -p /plugins/plugin-1/button-icon -n -t string -s "desktop-environment-xfce"
 xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-filesystem -n -t bool -s false
 xfconf-query -c xfce4-notifyd -p /do-slideout -n -t bool -s true
 xfconf-query -c xfce4-notifyd -p /notify-location -n -t int -s 3
@@ -132,6 +132,7 @@ xfconf-query -c xfce4-notifyd -p /notification-log -n -t bool -s true
 xfconf-query -c xfce4-notifyd -p /log-level -n -t int -s 1
 xfconf-query -c xfce4-notifyd -p /log-max-size -n -t int -s 0
 xfconf-query -c xsettings -p /Xft/DPI -n -t int -s 100
+xfconf-query -c xsettings -p /Net/IconThemeName -n -t string -s "Papirus-Dark"
 #sudo sed -i 's/^#greeter-setup-script=.*/greeter-setup-script=\/usr\/bin\/numlockx on/' /etc/lightdm/lightdm.conf
 sudo cp lightdm-gtk-greeter.conf /etc/lightdm/
 sudo systemctl enable lightdm
@@ -139,8 +140,6 @@ sudo systemctl enable lightdm
 echo ""
 read -r -p "Do you want to install Papirus Icon Theme and Colloid GTK Theme? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    wget -qO- https://git.io/papirus-icon-theme-install | sh
-
     echo ""
     git clone https://github.com/vinceliuice/Colloid-gtk-theme.git --depth=1
     cd Colloid-gtk-theme/
@@ -148,7 +147,6 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     cd ..
     rm -rf Colloid-gtk-theme/
 
-    xfconf-query -c xsettings -p /Net/IconThemeName -n -t string -s "Papirus-Dark"
     xfconf-query -c xsettings -p /Net/ThemeName -n -t string -s "Colloid-Dark"
     xfconf-query -c xfwm4 -p /general/theme -n -t string -s "Colloid-Dark"
 fi

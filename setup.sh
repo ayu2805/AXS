aeaa533086c145ca1c269840c172a357109add6c
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-07-30 17:06:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -122,7 +122,7 @@ xfconf-query -c xfce4-panel -p /panels/panel-1/size -n -t int -s 32
 xfconf-query -c xfce4-panel -p /panels/panel-1/icon-size -n -t int -s 16
 xfconf-query -c xfce4-panel -p /plugins/plugin-1/show-button-title -n -t bool -s false
 xfconf-query -c xfce4-panel -p /plugins/plugin-1/button-icon -n -t string -s "desktop-environment-xfce"
-xfconf-query -c xfce4-panel -p /panels -n -t int -s 1 -a
+#xfconf-query -c xfce4-panel -p /panels -n -t int -s 1 -a
 xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-filesystem -n -t bool -s false
 xfconf-query -c xfce4-notifyd -p /do-slideout -n -t bool -s true
 xfconf-query -c xfce4-notifyd -p /notify-location -n -t int -s 3
